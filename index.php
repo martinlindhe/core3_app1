3739427bcc8038f22d3507474461105c47d8f348
Martin Lindhe <martin@ubique.se>
2014-05-23 10:18:52 +0200

    sync

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  */
 
 require_once 'core3/class/Core/Bootstrapper.php';
-spl_autoload_register('Core_Bootstrapper::autoload');
+spl_autoload_register('Core3\Core\Bootstrapper::autoload');
 
 $request = '';
 if (isset($_SERVER['REDIRECT_URL'])) {
@@ -13,7 +13,7 @@ if (isset($_SERVER['REDIRECT_URL'])) {
     $request = $_SERVER['REQUEST_URI'];
 }
 
-$router = new Web_RequestRouter();
+$router = new Core3\Web\RequestRouter();
 $router->setApplicationDirectoryRoot(__DIR__);
 $router->setApplicationWebRoot('/app1');  // XXX TODO FIXME how should we set this up properly? it is only needed when app is not in root vhost
 $router->route($request);
