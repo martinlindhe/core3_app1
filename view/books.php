0060ebae7f5755e5044aff76eb8735992741dd67
Martin Lindhe <martin@ubique.se>
2014-05-27 08:46:04 +0200

    sync changes

--- view/books.php
+++ view/books.php
@@ -29,29 +29,30 @@ TODO: pagination
 
 <body>
 
-<div ng-app="bookApp" ng-controller="MainController">
+<div ng-app="bookApp">
+	<div ng-controller="MainController">
 
-	<script src="js/books.js"></script>
+		<script src="js/books.js"></script>
 
-	<input ng-model="person.name" type="text" placeholder="Your name">
-	<h1>Hello {{ person.name }}</h1>
+		<input ng-model="person.name" type="text" placeholder="Your name">
+		<h1>Hello {{ person.name }}</h1>
 
-	<a href="/app1/books/Book/Moby">Moby</a> |
-	<a href="/app1/books/Book/Moby/ch/1">Moby: Ch1</a> |
-	<a href="/app1/books/Book/Gatsby">Gatsby</a> |
-	<a href="/app1/books/Book/Gatsby/ch/4?key=value">Gatsby: Ch4</a> |
-	<a href="/app1/books/Book/Scarlet">Scarlet Letter</a><br/>
+		<a href="/app1/books/Book/Moby">Moby</a> |
+		<a href="/app1/books/Book/Moby/ch/1">Moby: Ch1</a> |
+		<a href="/app1/books/Book/Gatsby">Gatsby</a> |
+		<a href="/app1/books/Book/Gatsby/ch/4?key=value">Gatsby: Ch4</a> |
+		<a href="/app1/books/Book/Scarlet">Scarlet Letter</a><br/>
 
-	<div ng-view></div>
+		<div ng-view></div>
 
-	<hr />
+		<hr />
 
-	<pre>$location.path() = {{$location.path()}}</pre>
-	<pre>$route.current.templateUrl = {{$route.current.templateUrl}}</pre>
-	<pre>$route.current.params = {{$route.current.params}}</pre>
-	<pre>$route.current.scope.name = {{$route.current.scope.name}}</pre>
-	<pre>$routeParams = {{$routeParams}}</pre>
-
+		<pre>$location.path() = {{$location.path()}}</pre>
+		<pre>$route.current.templateUrl = {{$route.current.templateUrl}}</pre>
+		<pre>$route.current.params = {{$route.current.params}}</pre>
+		<pre>$route.current.scope.name = {{$route.current.scope.name}}</pre>
+		<pre>$routeParams = {{$routeParams}}</pre>
+	</div>
 </div>
 
 </body>
