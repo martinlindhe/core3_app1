48fee77678e0b60e56b3c94e331a1303cad50174
Martin Lindhe <martin.lindhe@freespee.com>
2014-05-28 14:59:28 +0200

    view/scss: implemented proper caching using Last-Modified and Etag server headers

--- README.md
+++ README.md
@@ -40,5 +40,3 @@ Create a symlink for angular.js to be accessable in the web root:
 
 later: composer dependency for core3 ?
 
-Makefile: "tidy": TODO print file name before each call to tidy, how?!
-
--- view/scss.php
+++ view/scss.php
@@ -17,11 +17,31 @@ function isValidScssFileName($name) // XXX MOVE TO CLASS
 	return false;
 }
 
+function sendValidationHeaders($mtime, $etag)
+{
+	header('Last-Modified: '.gmdate('D, j M Y H:i:s', $mtime).' GMT');
+	header('Etag: '.$etag);
+	header('Cache-Control: public'); // XXX useless?
+}
+
+function isModified($mtime, $etag)
+{
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) < $mtime) {
+		return true;
+	}
+
+	if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] != $etag) {
+		return true;
+	}
+
+	return false;
+}
+
 if ($param[0] == 'compile') {
 	// $param[1] = name of scss file
 
 	if (!isValidScssFileName($param[1])) {
-		http_response_code(400);
+		http_response_code(400); // Bad Request
 		header('Content-Type: application/json');
 		echo json_encode(array('code'=>400, 'message'=>'Invalid scss name'));
 		exit;
@@ -35,7 +55,7 @@ if ($param[0] == 'compile') {
 
 	if (!file_exists($scssFile)) {
 		// TODO refactor API error message
-		http_response_code(400);
+		http_response_code(400); // Bad Request
 		header('Content-Type: application/json');
 		echo json_encode(array('code'=>400, 'message'=>'No such scss file'));
 		exit;
@@ -48,12 +68,29 @@ if ($param[0] == 'compile') {
 		$cachedMtime = filemtime($cachedFile);
 	}
 
+	$etag = md5($scssFile.$cachedMtime);
+
 	header('Content-Type: text/css');
 
 	if ($cachedMtime > $scssMtime) {
-		// serve cached copy
-		readfile($cachedFile);
+
+		sendValidationHeaders($cachedMtime, $etag);
+
+		if (isModified($cachedMtime, $etag)) {
+			// serve cached copy if browser didnt have it cached
+			readfile($cachedFile);
+		} else {
+			http_response_code(304);   // Not Modified
+			exit();
+		}
+
 	} else {
+		if (!$cachedMtime) {
+			$cachedMtime = time();
+		}
+
+		sendValidationHeaders($cachedMtime, $etag);
+
 		$scss->setImportPaths($scssPath);
 		$scss->setFormatter('scss_formatter_compressed');
 
