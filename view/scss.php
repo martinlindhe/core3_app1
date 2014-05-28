c23ae2c11816fd7f367bc547f1e5dbc88e8b1f2b
Martin Lindhe <martin.lindhe@freespee.com>
2014-05-28 15:44:46 +0200

    refactored scss rendering to core3 Writer\Scss class

--- view/scss.php
+++ view/scss.php
@@ -5,61 +5,8 @@
 
 $viewName = $param[0]; // base name of the scss file
 
-if (!\Writer\Scss::isValidScssFileName($viewName)) {
-	http_response_code(400); // Bad Request
-	header('Content-Type: application/json');
-	echo json_encode(array('code'=>400, 'message'=>'Invalid scss name'));
-	exit;
-}
+// XXX untangle http response codes / api responses from Scss class
+$scss = new \Writer\Scss();
 
-$scss = \Writer\Scss::getInstance();
-
-$scssPath = realpath(__DIR__.'/../scss');
-$scssFile = $scssPath.'/'.$viewName.'.scss';
-$cachedFile = $scssPath.'/'.$viewName.'.compiled.css';
-
-if (!file_exists($scssFile)) {
-	// TODO refactor API error message
-	http_response_code(400); // Bad Request
-	header('Content-Type: application/json');
-	echo json_encode(array('code'=>400, 'message'=>'No such scss file'));
-	exit;
-}
-
-$scssMtime = filemtime($scssFile);
-
-$cachedMtime = 0;
-if (file_exists($cachedFile)) {
-	$cachedMtime = filemtime($cachedFile);
-}
-
-$etag = md5($scssFile.$cachedMtime);
-
-header('Content-Type: text/css');
-
-if ($cachedMtime > $scssMtime) {
-
-	\Writer\Scss::sendValidationHeaders($cachedMtime, $etag);
-
-	if (\Writer\Scss::isModified($cachedMtime, $etag)) {
-		// serve cached copy if browser didnt have it cached
-		readfile($cachedFile);
-	} else {
-		http_response_code(304);   // Not Modified
-		exit();
-	}
-
-} else {
-	if (!$cachedMtime) {
-		$cachedMtime = time();
-	}
-
-	\Writer\Scss::sendValidationHeaders($cachedMtime, $etag);
-
-	$scss->setImportPaths($scssPath);
-	$scss->setFormatter('scss_formatter_compressed');
-
-	$data = $scss->compile('@import "'.basename($scssFile).'"');
-	echo $data;
-	file_put_contents($cachedFile, $data);
-}
+$scss->setImportPath(realpath(__DIR__.'/../scss'));
+$scss->handle($viewName);
