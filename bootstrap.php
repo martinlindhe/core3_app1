7f979037dcdb688292bc7ffcac882af917d40531
Martin Lindhe <martin@ubique.se>
2014-06-18 16:42:15 +0200

    add project autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,16 @@
 <?php
-namespace Core;
 
 require_once __DIR__.'/core3/class/Core/Bootstrap.php';
 
-Bootstrap::registerAutoloader();
+// register core3 autoloader
+\Core\Bootstrap::registerAutoloader();
+
+// register project autoloader
+spl_autoload_register(function ($class) {
+    $class = strtr($class, "\\", DIRECTORY_SEPARATOR);
+    $fileName = (__DIR__.'/class').'/'.$class.'.php';
+
+    if (file_exists($fileName)) {
+        include $fileName;
+    }
+});
