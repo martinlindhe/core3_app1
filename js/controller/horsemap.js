713755633097140c20a6c37e16f57458080a5dbd
Martin Lindhe <martin@ubique.se>
2014-07-10 02:18:31 +0200

    angular-slider: add local copy from https://github.com/prajwalkman/angular-slider

--- js/controller/horsemap.js
+++ js/controller/horsemap.js
@@ -1,11 +1,14 @@
 'use strict';
 
 Date.prototype.yyyymmdd = function() {
-   var yyyy = this.getFullYear().toString();
-   var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-   var dd  = this.getDate().toString();
-   return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
-  };
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+    var dd = this.getDate().toString();
+
+    return yyyy +
+        (mm[1] ? mm : "0" + mm[0]) +
+        (dd[1] ? dd : "0" + dd[0]); // padding
+};
 
 angular.module('horseMap', ['google-maps'])
     .controller('GoogleMapController', function($scope, $http, $log) {
@@ -30,6 +33,7 @@ angular.module('horseMap', ['google-maps'])
 
         $scope.unixTime = 1400623200; // 2014-05-21 00:00:00;
         var date = new Date($scope.unixTime * 1000);
+        $log.info('yyyymmdd ' + date.yyyymmdd());
 
         /// TODO Start date 20140521, end 20140701
 
