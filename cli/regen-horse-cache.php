fe688447c8980a8768671323b343ecfe5bd5d534
Martin Lindhe <martin@ubique.se>
2014-07-10 22:07:49 +0200

    horse cache: generate for all available durations

--- cli/regen-horse-cache.php
+++ cli/regen-horse-cache.php
@@ -7,19 +7,20 @@
 // TODO calc all durations
 $horses = [4664, 4665];
 $startTime = strtotime('20140520');
-$regenAllFiles = true;
+$endTime = strtotime('20140701');
+$regenAllFiles = false;
 
 
+$availableDurations = [3600, 3600*4, 3600*8, 3600*24];
 
-$durationSeconds = 60 * 60 * 24; // 24h
+foreach ($availableDurations as $durationSeconds) {
+    for ($currentTime = $startTime; $currentTime <= $endTime; $currentTime += $durationSeconds) {
+        //echo "Generating ".date('r', $currentTime).", ".($durationSeconds / 3600)." hours\n";
 
-
-for ($currentTime = $startTime; $currentTime < ($startTime + ($durationSeconds * 10) ); $currentTime += $durationSeconds) {
-    echo "Generating for date ".date('r', $currentTime).", ".($durationSeconds / 3600)." hours\n";
-
-    foreach ($horses as $horse) {
-        if (WriterHorseDataCache::generate($horse, $currentTime, $durationSeconds, $regenAllFiles)) {
-            echo "Generated cache ".$horse.'-'.$currentTime.'-'.$durationSeconds."\n";
+        foreach ($horses as $horse) {
+            if (WriterHorseDataCache::generate($horse, $currentTime, $durationSeconds, $regenAllFiles)) {
+                echo "Generated cache ".$horse.'-'.$currentTime.'-'.$durationSeconds."\n";
+            }
         }
     }
 }
