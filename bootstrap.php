a069c874f2c1eaf5a8d58592c8c5d61a8017e91e
Martin Lindhe <martin@ubique.se>
2014-09-12 18:20:20 +0200

    update for core3 autoloading

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 
 // register core3 autoloader
-spl_autoload_register(function ($class)
-{
-    $class = strtr($class, "\\", DIRECTORY_SEPARATOR);
-    $fileName = __DIR__.'/core3/class/'.$class.'.php';
-
-    if (file_exists($fileName)) {
-        include $fileName;
-    }
-});
+require_once __DIR__.'/core3/vendor/autoload.php';
 
 
 // register project autoloader
